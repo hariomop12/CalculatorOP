22042f99146a7d4f13a3dff7502a9bbe47360f8f
hariomop12 <Hariomvirkhare02@gmail.com>
2024-09-03 18:16:42 +0530

    Updated

--- app/src/main/java/com/example/calculatorop/ui/theme/Onboarding/sample.kt
+++ app/src/main/java/com/example/calculatorop/ui/theme/Onboarding/sample.kt
@@ -1,12 +1,20 @@
-//package com.example.calculatorop.ui.theme.Onboarding
-//
-//import androidx.compose.foundation.layout.*
-//import androidx.compose.foundation.text.KeyboardActions
-//import androidx.compose.foundation.text.KeyboardOptions
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//
-//@Composable
-//fun jhe(modifier: Modifier = Modifier) {
-//
-//}
+package com.example.calculatorop.ui.theme.Onboarding
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun jhe(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text("Hello, World!")
+    }
+}
